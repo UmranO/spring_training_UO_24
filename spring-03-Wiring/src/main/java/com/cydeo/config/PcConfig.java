502cc1a034cf6b0a44aae98acfa3122d6fb80e49
Umran Ozman <umonal@yahoo.com>
2024-12-27 17:51:10 -0500

    PcConfig Class created

---
+++ spring-03-Wiring/src/main/java/com/cydeo/config/PcConfig.java
@@ -0,0 +1,13 @@
+package com.cydeo.config;
+
+import com.cydeo.casefactory.Case;
+import com.cydeo.casefactory.DellCase;
+import com.cydeo.casefactory.Dimensions;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Configuration
+public class PcConfig {
+
+}
