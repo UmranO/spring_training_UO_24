e7b21590ce783e97aea60bd71e166d6d9a8f0afd
Umran Ozman <umonal@yahoo.com>
2024-12-27 20:45:16 -0500

    ComputerTest Class created

---
+++ spring-03-Wiring/src/main/java/com/cydeo/ComputerTest.java
@@ -0,0 +1,11 @@
+package com.cydeo;
+
+import com.cydeo.casefactory.Case;
+import com.cydeo.config.PcConfig;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ComputerTest {
+
+
+}
