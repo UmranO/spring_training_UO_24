3d0a305bf596f5930b3a6e49a30d66238376484c
Umran Ozman <umonal@yahoo.com>
2024-12-27 20:44:23 -0500

    PcConfig Class updated

--- spring-03-Wiring/src/main/java/com/cydeo/config/PcConfig.java
+++ spring-03-Wiring/src/main/java/com/cydeo/config/PcConfig.java
@@ -10,4 +10,15 @@ import org.springframework.context.annotation.Primary;
 @Configuration
 public class PcConfig {
 
+    @Bean
+    public Dimensions dimensions(){
+        return new Dimensions(50,10,10);
+    }
+
+    @Bean
+    public Case caseDell(Dimensions dimensions){
+        return new DellCase("220B","Dell","240",dimensions);
+
+    }
+
 }
